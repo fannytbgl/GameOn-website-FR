2ad6923f9b9f40a17fd7f14ca6409e6d85a950bd
ftbgl <tabaglio.fanny@gmail.com>
2023-12-21 09:51:17 +0100

    organise layers in project

--- js/modal.js
+++ js/modal.js
@@ -9,15 +9,23 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const modalCloseBtn = document.querySelector(".close");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+//close modal event (Issue #1)
+modalCloseBtn.addEventListener("click", (event) => {
+  modalbg.style.display = "none";
+})
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+
 
 
